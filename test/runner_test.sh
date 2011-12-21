859fe606c1acc8413b78b748b410d525ab9623a9
Ryan Brainard <brainard@heroku.com>
2011-12-21 12:14:38 -0800

    Aggregate exit codes by taking the max of the indidvual exit codes and associated tests for the test runner itself.

---
+++ test/runner_test.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+. ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
+
+testValidBuildpackWithTests()
+{
+  mkdir -p ${OUTPUT_DIR}/valid_buildpack/test
+  touch ${OUTPUT_DIR}/valid_buildpack/test/sample_test.sh
+
+  capture ${BUILDPACK_TEST_RUNNER_HOME}/bin/run ${OUTPUT_DIR}/valid_buildpack
+  assertContains "Running test suite: ${OUTPUT_DIR}/valid_buildpack" "$(cat ${STD_OUT})"
+  assertContains "Ran 0 tests." "$(cat ${STD_OUT})"
+  assertEquals "" "$(cat ${STD_ERR})"
+  assertEquals "0" "${rtrn}"
+}
+
+testInvalidBuildpackDirectory()
+{
+  capture ${BUILDPACK_TEST_RUNNER_HOME}/bin/run ${OUTPUT_DIR}/invalid_buildpack
+  assertEquals "" "$(cat ${STD_OUT})"
+  assertEquals "${OUTPUT_DIR}/invalid_buildpack is not a directory" "$(cat ${STD_ERR})"
+  assertEquals "1" "${rtrn}"
+}
+
+
+testInvalidBuildpackDoesNotContainTestDirectory()
+{
+  mkdir ${OUTPUT_DIR}/buildpack_without_tests
+
+  capture ${BUILDPACK_TEST_RUNNER_HOME}/bin/run ${OUTPUT_DIR}/buildpack_without_tests
+  assertEquals "" "$(cat ${STD_OUT})"
+  assertEquals "Buildpack '${OUTPUT_DIR}/buildpack_without_tests' does not contain valid tests. Must contain a 'test' directory and with files matching '*_test.sh'" "$(cat ${STD_ERR})"
+  assertEquals "1" "${rtrn}"
+}
+
+testInvalidBuildpackDoesNotContainTests()
+{
+  mkdir -p ${OUTPUT_DIR}/buildpack_without_tests/test
+  touch ${OUTPUT_DIR}/buildpack_without_tests/test/non_test_file.sh
+
+  capture ${BUILDPACK_TEST_RUNNER_HOME}/bin/run ${OUTPUT_DIR}/buildpack_without_tests
+  assertEquals "" "$(cat ${STD_OUT})"
+  assertEquals "Buildpack '${OUTPUT_DIR}/buildpack_without_tests' does not contain valid tests. Must contain a 'test' directory and with files matching '*_test.sh'" "$(cat ${STD_ERR})"
+  assertEquals "1" "${rtrn}"
+}
+
+testValidBuildpackWithTestsAndThenInvalidBuildpackWithoutTests()
+{
+  mkdir -p ${OUTPUT_DIR}/valid_buildpack/test
+  touch ${OUTPUT_DIR}/valid_buildpack/test/sample_test.sh
+
+  capture ${BUILDPACK_TEST_RUNNER_HOME}/bin/run ${OUTPUT_DIR}/valid_buildpack ${OUTPUT_DIR}/invalid_buildpack
+  assertContains "Running test suite: ${OUTPUT_DIR}/valid_buildpack" "$(cat ${STD_OUT})"
+  assertContains "Ran 0 tests." "$(cat ${STD_OUT})"
+  assertEquals "${OUTPUT_DIR}/invalid_buildpack is not a directory" "$(cat ${STD_ERR})"
+  assertEquals "1" "${rtrn}"
+}
+
+testInvalidBuildpackWithoutTestsAndThenValidBuildpackWithTests()
+{
+  mkdir -p ${OUTPUT_DIR}/valid_buildpack/test
+  touch ${OUTPUT_DIR}/valid_buildpack/test/sample_test.sh
+
+  capture ${BUILDPACK_TEST_RUNNER_HOME}/bin/run ${OUTPUT_DIR}/invalid_buildpack ${OUTPUT_DIR}/valid_buildpack
+  assertContains "Running test suite: ${OUTPUT_DIR}/valid_buildpack" "$(cat ${STD_OUT})"
+  assertContains "Ran 0 tests." "$(cat ${STD_OUT})"
+  assertEquals "${OUTPUT_DIR}/invalid_buildpack is not a directory" "$(cat ${STD_ERR})"
+  assertEquals "1" "${rtrn}"
+}
+
+testNoArgsPrintsUsage()
+{
+  capture ${BUILDPACK_TEST_RUNNER_HOME}/bin/run
+  assertContains "Usage" "$(cat ${STD_OUT})"
+  assertEquals "" "$(cat ${STD_ERR})"
+  assertEquals "1" "${rtrn}"
+}
